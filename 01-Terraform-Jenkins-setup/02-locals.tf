ad88732c5264e38f399c65859f147288ffd30cb2
Ksab070 <khansubhan070@gmail.com>
2025-04-04 17:48:55 +0530

    Removed secret

--- 01-Terraform-Jenkins-setup/02-locals.tf
+++ 01-Terraform-Jenkins-setup/02-locals.tf
@@ -1,7 +1,7 @@
 locals {
   region     = "us-east-1"
-  access_key = "AKIAXYKJUF3N5UJLK7C6"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   aws_tags = {
     "Terraform"   = "True"
     "Purpose"     = "Jenkins-infra"
