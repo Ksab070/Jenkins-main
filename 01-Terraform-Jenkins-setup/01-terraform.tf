f6873570e78368bb162008e4a886c0984a94bbb6
Ksab070 <khansubhan070@gmail.com>
2025-04-05 14:12:52 +0530

    Removed secrets way from main code, will use aws configure from now on

--- 01-Terraform-Jenkins-setup/01-terraform.tf
+++ 01-Terraform-Jenkins-setup/01-terraform.tf
@@ -17,8 +17,6 @@ terraform {
 
 provider "aws" {
   region     = local.region
-  access_key = local.access_key
-  secret_key = local.secret_key
 }
 
 
--- 01-Terraform-Jenkins-setup/02-locals.tf
+++ 01-Terraform-Jenkins-setup/02-locals.tf
@@ -1,7 +1,5 @@
 locals {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
   aws_tags = {
     "Terraform"   = "True"
     "Purpose"     = "Jenkins-infra"
